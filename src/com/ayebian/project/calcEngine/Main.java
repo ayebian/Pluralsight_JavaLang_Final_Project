551c151393e5ac5de60696cb53e55d4234f0ba39
ayebian <ayebian16295@gmail.com>
2019-01-12 15:42:38 +0300

    Hidden field declarations using Accessors and Mutators

--- src/com/ayebian/project/calcEngine/Main.java
+++ src/com/ayebian/project/calcEngine/Main.java
@@ -14,7 +14,7 @@ public class Main {
         for (MathEquation equation:equations){
             equation.execute();
             System.out.print("Result = ");
-            System.out.println(equation.result);
+            System.out.println(equation.getResult());
 
         }
 
@@ -23,9 +23,9 @@ public class Main {
 
     public static MathEquation create(double leftVal, double rightVal, char opCode)  {
         MathEquation equation = new MathEquation();
-        equation.leftVal = leftVal;
-        equation.rightVal = rightVal;
-        equation.opCode = opCode;
+        equation.setLeftVal(leftVal);
+        equation.setRightVal(rightVal);
+        equation.setOpCode(opCode);
 
         return equation;
     }
--- src/com/ayebian/project/calcEngine/MathEquation.java
+++ src/com/ayebian/project/calcEngine/MathEquation.java
@@ -3,10 +3,44 @@ package com.ayebian.project.calcEngine;
 //Created by Ayebare Ian on 12/01/2018
 
 public class MathEquation {
-    public double leftVal;
-    public double rightVal;
-    public double result;
-    public char opCode;
+    private double leftVal;
+    private double rightVal;
+    private double result;
+    private char opCode;
+
+
+    public double getLeftVal(){
+        return leftVal;
+    }
+
+    public void setLeftVal(double leftVal){
+        this.leftVal = leftVal;
+    }
+
+
+    public double getRightVal(){
+        return rightVal;
+    }
+
+    public void setRightVal(double rightVal){
+        this.rightVal = rightVal;
+    }
+
+    public double getResult(){
+        return result;
+    }
+
+    public void setResult(double result){
+        this.leftVal = result;
+    }
+
+    public char getOpCode(){
+        return opCode;
+    }
+
+    public void setOpCode(char opCode){
+        this.opCode = opCode;
+    }
 
 
     public void execute (){
