cc0540f78bf2a2f8d6b064d248309a58b82c076e
ayebian <ayebian16295@gmail.com>
2019-01-12 17:46:06 +0300

    Used specified Classes to enhance the apps performance

--- src/com/ayebian/project/calcEngine/Main.java
+++ src/com/ayebian/project/calcEngine/Main.java
@@ -4,18 +4,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MathEquation[] equations = new MathEquation[4];
-        equations[0]= new MathEquation(100.0d,50.0d,'d');
-        equations[1]= new MathEquation(25.0d,92.0d,'a');
-        equations[2]= new MathEquation(225.0d,17.0d,'s');
-        equations[3]= new MathEquation(11.0d,3.0d,'m');
+//        MathEquation[] equations = new MathEquation[4];
+//        equations[0]= new MathEquation(100.0d,50.0d,'d');
+//        equations[1]= new MathEquation(25.0d,92.0d,'a');
+//        equations[2]= new MathEquation(225.0d,17.0d,'s');
+//        equations[3]= new MathEquation(11.0d,3.0d,'m');
 
 
-        for (MathEquation equation:equations){
-            equation.execute();
+//        for (MathEquation equation:equations){
+//            equation.execute();
+//            System.out.print("Result = ");
+//            System.out.println(equation.getResult());
+//        }
+
+
+        CalculateBase[] calculators = {
+                new Divider(100.0d,50.0d),
+                new Adder( 25.0d, 92.0d),
+                new Subtractor(225.0d,17.0d),
+                new Multiplier(11.0d, 3.0d)
+        };
+
+        for (CalculateBase calculator:calculators){
+            calculator.calculate();
             System.out.print("Result = ");
-            System.out.println(equation.getResult());
+            System.out.println(calculator.getResult());
         }
+
+
+
+
     }
 
 }
--- src/com/ayebian/project/calcEngine/MathEquation.java
+++ src/com/ayebian/project/calcEngine/MathEquation.java
@@ -3,73 +3,64 @@ package com.ayebian.project.calcEngine;
 //Created by Ayebare Ian on 12/01/2018
 
 public class MathEquation {
-    private double leftVal;
-    private double rightVal;
-    private double result;
-    private char opCode;
+//    private double leftVal;
+//    private double rightVal;
+//    private double result;
+//    private char opCode;
+//
+//
+//    public double getLeftVal(){return leftVal;}
+//
+//    public void setLeftVal(double leftVal){this.leftVal = leftVal;}
+//
+//    public double getRightVal(){ return rightVal;}
+//
+//    public void setRightVal(double rightVal){this.rightVal = rightVal; }
+//
+//    public double getResult(){return result;}
+//
+//    public void setResult(double result){this.leftVal = result; }
+//
+//    public char getOpCode(){return opCode;}
+//
+//    public void setOpCode(char opCode){this.opCode = opCode;}
 
 
-    public double getLeftVal(){
-        return leftVal;
-    }
-
-    public void setLeftVal(double leftVal){
-        this.leftVal = leftVal;
-    }
-
-
-    public double getRightVal(){
-        return rightVal;
-    }
-
-    public void setRightVal(double rightVal){
-        this.rightVal = rightVal;
-    }
-
-    public double getResult(){
-        return result;
-    }
-
-    public void setResult(double result){
-        this.leftVal = result;
-    }
-
-    public char getOpCode(){
-        return opCode;
-    }
-
-    public void setOpCode(char opCode){
-        this.opCode = opCode;
-    }
-
-
-    public MathEquation(double leftVal, double rightVal, char opCode){
-        this.rightVal = rightVal;
-        this.leftVal = leftVal;
-        this.opCode = opCode;
-    }
-
-
-    public void execute (){
-        switch (opCode){
-            case 'a':
-                result = leftVal + rightVal;
-                break;
-            case  's':
-                result = leftVal - rightVal;
-                break;
-            case 'd':
-                result= (rightVal != 0.0d) ? leftVal / rightVal : 0.0d;
-                break;
-            case 'm':
-                result = rightVal * leftVal;
-                break;
-
-            default:
-                System.out.println("Error -Invalid opCode");
-                result =0.0d;
-        }
-    }
+//    public MathEquation(){
+//    }
+//
+//    public MathEquation(char opCode){
+//        this.opCode = opCode;
+//    }
+//
+//    public MathEquation(double leftVal, double rightVal, char opCode){
+//        this(opCode);
+//        this.rightVal = rightVal;
+//        this.leftVal = leftVal;
+//        this.opCode = opCode;
+//    }
+//
+//
+//    public void execute (){
+//        switch (opCode){
+//            case 'a':
+//                result = leftVal + rightVal;
+//                break;
+//            case  's':
+//                result = leftVal - rightVal;
+//                break;
+//            case 'd':
+//                result= (rightVal != 0.0d) ? leftVal / rightVal : 0.0d;
+//                break;
+//            case 'm':
+//                result = rightVal * leftVal;
+//                break;
+//
+//            default:
+//                System.out.println("Error -Invalid opCode");
+//                result =0.0d;
+//        }
+//    }
 
 
 
